0ce6bf4d7f8fc3dd545d637e5d92ced6988bade7
jaydonhashimoto <jaydonhashi@gmail.com>
2019-05-15 21:50:45 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const keys = require('./config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+//bodyParser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//handlebars middelware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//set static folder
+app.use(express.static(`${__dirname}/public`));
+
+//index route
+app.get('/', (req, res) => {
+    //render index page with pk value
+    res.render('index', {
+        stripePublishableKey: keys.stripePublishableKey
+    });
+});
+
+//charge route
+app.post('/charge', (req, res) => {
+    //define amount
+    const amount = 2500;
+    //create customer
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    })
+        //charge customer
+        .then(customer => stripe.charges.create({
+            amount: amount,
+            description: 'Web Development Ebook',
+            currency: 'usd',
+            customer: customer.id
+        }))
+        //render success page
+        .then(charge => res.render('success'));
+});
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
